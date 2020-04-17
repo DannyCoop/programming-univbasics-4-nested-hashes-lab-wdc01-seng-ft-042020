8fb250910f57a9f74174e0c7ff8ec4fb1b995f74
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-17 05:33:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-	(programmer_hash[:dennis_ritchies][:languages]).first
+	programmer_hash[:dennis_ritchies][:languages].index(0)
 end
 
 def adding_matz
